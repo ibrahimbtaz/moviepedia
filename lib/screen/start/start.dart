c0d80a1f97ec5fedc10e42684067071692073f0d
ibrahimbtaz <ibrahimbtaz@gmail.com>
2023-01-22 15:50:51 +0700

    add : [SI] add start page

---
+++ lib/screen/start/start.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class Start extends StatefulWidget {
+  const Start({super.key});
+
+  @override
+  State<Start> createState() => _StartState();
+}
+
+class _StartState extends State<Start> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        // decoration: BoxDecoration(
+        //     image: DecorationImage(
+        //         image: AssetImage(Config.splash_bg), fit: BoxFit.cover)),
+
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 600,
+            ),
+            Text(
+              "Let's Cook",
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyText1!
+                  .copyWith(fontSize: 24, overflow: TextOverflow.fade),
+            ),
+            Text(
+              "Find The Best Recipe",
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyText1!
+                  .copyWith(fontSize: 24, overflow: TextOverflow.fade),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              height: 60,
+              width: 250,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => LoginScreen()),
+                  // );
+                },
+                style: ElevatedButton.styleFrom(
+                    // backgroundColor: HexColor(ColoR.primary),
+                    ),
+                child: Text(
+                  "Start Cook",
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText1!
+                      .copyWith(fontSize: 24, overflow: TextOverflow.fade),
+                ),
+              ),
+            ),
+            const Expanded(child: SizedBox()),
+          ],
+        )),
+      ),
+    );
+  }
+}
